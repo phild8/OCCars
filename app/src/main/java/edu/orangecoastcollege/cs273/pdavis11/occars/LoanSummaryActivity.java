58cba12833078ad59a5242fd72205769b47e468b
Phil <pjsdavs@gmail.com>
2017-09-21 13:07:41 -0700

    Added RadioButtons and Button

--- app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/LoanSummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/LoanSummaryActivity.java
@@ -3,13 +3,18 @@ package edu.orangecoastcollege.cs273.pdavis11.occars;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class LoanSummaryActivity extends AppCompatActivity {
+
+    private TextView mMontlyPaymentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_summary);
+
+        mMontlyPaymentTextView = (TextView) findViewById(R.id.MonthlyPaymentTextView);
 
         // Receive the intent (from PurchaseActivity)
         // Populate the TextView with data
@@ -18,6 +23,7 @@ public class LoanSummaryActivity extends AppCompatActivity {
         String report = intentFromPurchase.getStringExtra("loanReport");
 
         // Fill your TextView with data from the report
-
+        mMontlyPaymentTextView.setText(report);
     }
+
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/PurchaseActivity.java
@@ -3,6 +3,8 @@ package edu.orangecoastcollege.cs273.pdavis11.occars;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -32,12 +34,82 @@ public class PurchaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_purchase);
 
         // Use findViewById to connect controller to each view
+        // TextView/EditView
         mPricedEditText = (EditText) findViewById(R.id.carPriceEditText);
         mdDownPaymentEditText = (EditText) findViewById(R.id.downPaymentEditText);
-        mThreeYearRadioButton = (RadioButton) findViewById(R.id.threeYearRadioButton);
-        mFourYearRadioButton = (RadioButton) findViewById(R.id.fourYearRadioButton);
-        mFiveYearRadioButton = (RadioButton) findViewById(R.id.fiveYearRadioButton);
+
+        // Loan terms RadioButtons
+        mThreeYearRadioButton = (RadioButton) findViewById(R.id.threeYearButton);
+        mFourYearRadioButton = (RadioButton) findViewById(R.id.fourYearButton);
+        mFiveYearRadioButton = (RadioButton) findViewById(R.id.fiveYearButton);
+
+        // Price and down payment Text Watchers
+        mPricedEditText.addTextChangedListener(carPriceTextWatcher);
+        mdDownPaymentEditText.addTextChangedListener(downPaymentTextWatcher);
+
     }
+
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()){
+            case R.id.threeYearButton:
+                if (checked)
+                    mThreeYearRadioButton.isChecked();
+                    break;
+            case R.id.fourYearButton:
+                if (checked)
+                    mFourYearRadioButton.isChecked();
+                    break;
+            case R.id.fiveYearButton:
+                if (checked)
+                    mFiveYearRadioButton.isChecked();
+                    break;
+        }
+    }
+
+    private final TextWatcher carPriceTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence text, int i, int i1, int i2) {
+            try{
+                double carPrice = Double.parseDouble(text.toString());
+
+                mCarLoan.setPrice(carPrice);
+
+
+            } catch (NumberFormatException e){
+                mCarLoan.setPrice(0);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    };
+
+    private final TextWatcher downPaymentTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence text, int i, int i1, int i2) {
+            try {
+                double downPayment = Double.parseDouble(text.toString());
+
+                mCarLoan.setDownPayment(downPayment);
+            } catch (NumberFormatException e) {
+                mCarLoan.setPrice(0);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
 
     private void collectCarLoanData()
     {
@@ -56,12 +128,19 @@ public class PurchaseActivity extends AppCompatActivity {
     protected void reportSummary(View v)
     {
         collectCarLoanData();
-        String report = "Monthly Payment: " + mCarLoan.monthlyPayment() + "\n";
-        // keep going, more report!!
+
+        String report = "Monthly Payment: " + mCarLoan.monthlyPayment() + "\n\n";
+        report += "Car Sticker Price:  $" + mCarLoan.getPrice() + "\n";
+        report += "You will put down:  $" + mCarLoan.getDownPayment() + "\n";
+        report += "Taxed Amt:  $" + mCarLoan.taxAmount() + "\n";
+        report += "Your Cost:  $" + mCarLoan.totalAmount() + "\n";
+        report += "Borrowed Amount:  $" + mCarLoan.borrowedAmount() + "\n";
+        report += "Interest Amount:  $" + mCarLoan.interestAmount() + "\n";
+
 
         // Intents start new activities and can share data with them
         Intent LaunchLoanSummary = new Intent(this, LoanSummaryActivity.class);
-        // Put data into the Intent for LoanSummary to recieve
+        // Put data into the Intent for LoanSummary to receive
         LaunchLoanSummary.putExtra("loanReport", report);
         // Start the second activity
         startActivity(LaunchLoanSummary);
