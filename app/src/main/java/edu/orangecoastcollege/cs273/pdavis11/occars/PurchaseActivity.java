8addc4240ef19122fd09a65d44ac9ccec051380d
Phil <pjsdavs@gmail.com>
2018-08-02 21:30:45 -0700

    Debugged code. Updated Gradle.

--- app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/pdavis11/occars/PurchaseActivity.java
@@ -3,9 +3,15 @@ package edu.orangecoastcollege.cs273.pdavis11.occars;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 import java.text.NumberFormat;
 
 /**
@@ -44,32 +50,43 @@ public class PurchaseActivity extends AppCompatActivity {
         mFourYearRadioButton = findViewById(R.id.fourYearButton);
         mFiveYearRadioButton = findViewById(R.id.fiveYearButton);
     }
+    //Toast.makeText(PurchaseActivity.this, "Please enter a 'price' and a 'down payment' amount.", Toast.LENGTH_LONG).show();
 
-    public void collectCarLoanData(View view)
-    {
-        mCarLoan.setPrice(Double.parseDouble((mPricedEditText.getText().toString())));
-        mCarLoan.setDownPayment(Double.parseDouble((mdDownPaymentEditText.getText().toString())));
+    public void collectCarLoanData(View view) {
 
-        if (mThreeYearRadioButton.isChecked())
-            mCarLoan.setTerm(3);
-        else if (mFourYearRadioButton.isChecked())
-            mCarLoan.setTerm(4);
-        else if (mFiveYearRadioButton.isChecked())
-            mCarLoan.setTerm(5);
+        if (TextUtils.isEmpty(mPricedEditText.getText().toString()) && TextUtils.isEmpty(mdDownPaymentEditText.getText().toString()))
+            Toast.makeText(PurchaseActivity.this, "Please enter a 'price' and a 'down payment' amount.", Toast.LENGTH_LONG).show();
+        else if (TextUtils.isEmpty(mPricedEditText.getText().toString()))
+            Toast.makeText(PurchaseActivity.this, "Please enter a 'price' amount.", Toast.LENGTH_LONG).show();
+        else if (TextUtils.isEmpty(mdDownPaymentEditText.getText().toString()))
+            Toast.makeText(PurchaseActivity.this, "Please enter a 'down payment' amount.", Toast.LENGTH_LONG).show();
+        else if (!mThreeYearRadioButton.isChecked() && !mFourYearRadioButton.isChecked() && !mFiveYearRadioButton.isChecked())
+            Toast.makeText(PurchaseActivity.this, "Please select a 'term' amount.", Toast.LENGTH_LONG).show();
+        else {
+            mCarLoan.setPrice(Double.parseDouble((mPricedEditText.getText().toString())));
+            mCarLoan.setDownPayment(Double.parseDouble((mdDownPaymentEditText.getText().toString())));
+
+            if (mThreeYearRadioButton.isChecked())
+                mCarLoan.setTerm(3);
+            else if (mFourYearRadioButton.isChecked())
+                mCarLoan.setTerm(4);
+            else if (mFiveYearRadioButton.isChecked())
+                mCarLoan.setTerm(5);
+        }
     }
 
 
-    public void reportSummary(View view)
-    {
+    public void reportSummary(View view) {
+
         collectCarLoanData(view);
 
-        String report = "Monthly Payment: " + currency.format(mCarLoan.monthlyPayment()) + "\n\n"
-            + "Car Sticker Price:  $" + currency.format(mCarLoan.getPrice()) + "\n"
-            + "You will put down:  $" + currency.format(mCarLoan.getDownPayment()) + "\n"
-            + "Taxed Amt:  $" + currency.format(mCarLoan.taxAmount()) + "\n"
-            + "Your Cost:  $" + currency.format(mCarLoan.totalAmount()) + "\n"
-            + "Borrowed Amount:  $" + currency.format(mCarLoan.borrowedAmount()) + "\n"
-            + "Interest Amount:  $" + currency.format(mCarLoan.interestAmount()) + "\n";
+        String report = "Monthly Payment:  " + currency.format(mCarLoan.monthlyPayment()) + "\n\n"
+                      + "Car Sticker Price:  " + currency.format(mCarLoan.getPrice()) + "\n"
+                      + "You will put down:  " + currency.format(mCarLoan.getDownPayment()) + "\n"
+                      + "Taxed Amt:  " + currency.format(mCarLoan.taxAmount()) + "\n"
+                      + "Your Cost:  " + currency.format(mCarLoan.totalAmount()) + "\n"
+                      + "Borrowed Amount:  " + currency.format(mCarLoan.borrowedAmount()) + "\n"
+                      + "Interest Amount:  " + currency.format(mCarLoan.interestAmount()) + "\n";
 
 
         // Intents start new activities and can share data with them
